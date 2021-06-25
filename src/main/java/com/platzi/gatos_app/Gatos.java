5ef34bdcbaceb2434b6f1e8074c2835f700d59ca
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-25 18:29:50 -0500

    Initial Commit

---
+++ src/main/java/com/platzi/gatos_app/Gatos.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.platzi.gatos_app;
+
+/**
+ *
+ * @author diegobarrera
+ */
+public class Gatos {
+    int id;
+    String url;
+    String apikey = "5e968067-3494-4174-92fd-56f674624440";
+    String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getApikey() {
+        return apikey;
+    }
+
+    public void setApikey(String apikey) {
+        this.apikey = apikey;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+    
+}
